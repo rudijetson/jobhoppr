320143696f9cbd544c28ede2df0cb0fc75f361b8
rudijetson <info@engineer.marketing>
2024-07-07 18:04:10 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import RudiJetson from './playground';  // Import from playground.js
+
+function App() {
+  return (
+    <div className="App">
+      <RudiJetson />
+    </div>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        maroon: {
+          100: '#FFE6E6',
+          200: '#FFCCCC',
+          600: '#800000',
+          800: '#4d0000',
+        },
+      },
+    },
+  },
+  plugins: [],
+}
